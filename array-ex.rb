1b2c6196dd0377f8fadd130326ce19d954312869
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-26 22:36:08 -0300

    first commit

---
+++ array-ex.rb
@@ -0,0 +1,14 @@
+alunos = ['André', 'Sophia', 'Laura']
+notas = [5, 6, 8]
+puts("#{alunos[0]}  tirou  #{notas[1]}")
+puts("#{alunos[1]}  tirou  #{notas[1]}")
+puts("#{alunos[2]}  tirou  #{notas[2]}")
+
+alunos << 'Laura2' << 'Paulo'
+notas << 8.8 << 7.5
+
+puts("#{alunos[0]}  tirou  #{notas[1]}")
+puts("#{alunos[1]}  tirou  #{notas[1]}")
+puts("#{alunos[2]}  tirou  #{notas[2]}")
+puts("#{alunos[3]}  tirou  #{notas[3]}")
+puts("#{alunos[4]}  tirou  #{notas[4]}")
