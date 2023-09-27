e39b2903d583980d2bc24fb51819ae7c5fe3f05c
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-26 22:58:23 -0300

    exercicio hash

---
+++ hash-ex.rb
@@ -0,0 +1,3 @@
+alunos = [{nome: 'João', nota: 8, disciplina: 'Artes'},{nome: 'Alberto', nota: 7, disciplina: 'Artes'},{ nome: 'Ingrid', nota: 10, disciplina: 'Biologia' } ]
+puts(alunos[1][:nome]) 
+puts(alunos[1][:disciplina]) 
